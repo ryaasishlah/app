45c147b2dfef8d40f68655c5efc18bbf195f4929
rofinafiin <rofinafiisr@gmail.com>
2023-03-14 14:42:31 +0700

    tugas 5

---
+++ helpdesk/js/controller/croot.js
@@ -0,0 +1,24 @@
+import { addInner } from "https://jscroot.github.io/element/croot.js";
+import { get } from "https://jscroot.github.io/api/croot.js"; 
+import { getRandomColorName } from "./randomcol.js";
+import { rowtabel, svg, em } from "../template/tabel.js";
+get("https://gocroot-baru.herokuapp.com/hd", tabelContent);
+ 
+var isi = {};
+
+
+function tabelContent(result){
+    userTable(result);
+    isi = result;
+}
+
+function userTable(jsonParse){
+    let row = '';
+    jsonParse.forEach((element, index) => {
+    let svgicon = svg.replace("#WARNA#", getRandomColorName());
+    let emailcol = em.replace("#col#", getRandomColorName());
+    console.log(emailcol);
+    row = rowtabel.replace("#Nama#", element.user.nama).replace("#phonenumber#", element.user.handphone).replace("#namasistem#", element.sistemcomp).replace("#em#", emailcol).replace("#email#", element.user.email).replace("#status#", element.status).replace("#SVG#", svgicon);
+    addInner("demo",row);
+    });
+}
---
+++ helpdesk/js/controller/randomcol.js
@@ -0,0 +1,14 @@
+export function getRandomColorName() {
+    let colors = [
+        "yellow",
+        "green",
+        "blue",
+    ]
+    return colors[Math.floor(Math.random() * colors.length)];
+  }
+
+export function getSvg() {
+    let svg = {
+        
+    }
+}

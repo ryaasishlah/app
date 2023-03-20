fa23dc6fd3afb57817a2d3c1886f18ecd3a61c35
rofinafiin <rofinafiisr@gmail.com>
2023-03-20 09:15:29 +0700

    rename folder

--- helpdesk/js/controller/croot.js
+++ helpdesk/js/controller/croot.js
@@ -2,7 +2,8 @@ import { addInner } from "https://jscroot.github.io/element/croot.js";
 import { get } from "https://jscroot.github.io/api/croot.js"; 
 import { getRandomColorName } from "./randomcol.js";
 import { rowtabel, svg, em } from "../template/tabel.js";
-get("https://gocroot-baru.herokuapp.com/hd", tabelContent);
+import { URLHelpdesk } from "../config/url.js";
+get(URLHelpdesk, tabelContent);
  
 
 function tabelContent(result){
@@ -15,7 +16,13 @@ function userTable(jsonParse){
     let svgicon = svg.replace("#WARNA#", getRandomColorName());
     let emailcol = em.replace("#col#", getRandomColorName());
     console.log(emailcol);
-    row = rowtabel.replace("#Nama#", element.user.nama).replace("#phonenumber#", element.user.handphone).replace("#namasistem#", element.sistemcomp).replace("#em#", emailcol).replace("#email#", element.user.email).replace("#status#", element.status).replace("#SVG#", svgicon);
+    row = rowtabel.replace("#Nama#", element.user.nama).
+        replace("#phonenumber#", element.user.handphone).
+        replace("#namasistem#", element.sistemcomp).
+        replace("#em#", emailcol).
+        replace("#email#", element.user.email).
+        replace("#status#", element.status).
+        replace("#SVG#", svgicon);
     addInner("demo",row);
     });
 }

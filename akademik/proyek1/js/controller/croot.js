cf24ab90d0011dae2a01b7e2005d85e6ee8a778d
ryaasishlah <ryaasishlah123@gmail.com>
2023-07-03 15:19:36 +0700

    .

--- akademik/proyek1/js/controller/croot.js
+++ akademik/proyek1/js/controller/croot.js
@@ -1,7 +1,7 @@
 import { addInner } from "https://jscroot.github.io/element/croot.js";
 import { get } from "https://jscroot.github.io/api/croot.js"; 
 import { getRandomColorName } from "./randomcol.js";
-import {rowtabel, svg, pgr} from "../template/tabel.js";
+import {rowtabel, svg, hga} from "../template/tabel.js";
 import { URLProyek2 } from "../config/url.js";
 get(URLProyek2, tabelContent);
  
